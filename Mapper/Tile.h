f5c6daafad4a5bfec70a8c46500d547e4937453f
paweu9000 <wolfenorrin5@gmail.com>
2023-11-29 12:29:49 +0100

    initial TileBox Tile loading

--- Mapper/Tile.h
+++ Mapper/Tile.h
@@ -1,11 +1,13 @@
 #pragma once
 #include "Entity.h"
 #include "Program.h"
+#include <string>
+#include "SDL.h"
 
 class Tile : public Entity
 {
 public:
-	Tile(Program* program, const SDL_Rect& rect, int layer);
+	Tile(Program* program, const SDL_Rect rect, std::string fileName, int layer);
 	int getLayer() const { return mLayer; }
 private:
 	int mLayer;
